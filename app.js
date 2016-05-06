aaabcc490deb768deacd4720959b87ffc48d00b7
Nishant Jain <nishantkumarajain@gmail.com>
2016-05-06 07:44:31 +0000

    addedcompile

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module('myApp',[])
-  .directive('customBox',function() {
+  .directive('customBox',function($compile) {
     return {
       templateUrl: 'customBox.html',
       controller: function($scope) {
@@ -7,9 +7,12 @@ angular.module('myApp',[])
         console.log($scope);
       },
       link: function($scope,$element,$attr) {
-        console.log($scope);
+        // console.log($scope);
         console.log($element);
-        console.log($attr);
+        console.log($compile('<div>Hello</div><div ng-bind="name"></div>'));
+        $element.append(angular.element($compile('<div>Hello</div><div ng-bind="name"></div>')($scope)));
+
+        // console.log($attr);
       }
     }
   })
